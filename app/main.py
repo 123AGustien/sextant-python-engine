22218f6105b4c55f75c2c039be79ae3652148df0
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-18 23:00:23 +0700

    Rename and include user and auth routers

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from app.api.routes import router
+from app.api.routes import router as users_router
+from app.api.auth_routes import router as auth_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(users_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
