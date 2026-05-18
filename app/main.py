45779e636d1f44c55d5f9068642919b8f9a8c59e
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-19 00:32:01 +0700

    Move database initialization to startup event

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+
 from app.api.routes import router as users_router
 from app.api.auth_routes import router as auth_router
 from app.db.init_db import init_db
 
 app = FastAPI()
 
-init_db()  # creates database tables on startup
+
+@app.on_event("startup")
+def startup():
+    init_db()
+
 
 app.include_router(users_router)
 app.include_router(auth_router)
+
 
 @app.get("/")
 def home():
