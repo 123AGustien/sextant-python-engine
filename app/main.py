00782aac6844de3a047022bd0054d4021f26c67a
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-19 07:21:11 +0700

    Integrate API key middleware and routes
    Added API key middleware and routes for enhanced security.

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from app.db.init_db import init_db
 from app.api.auth_routes import router as auth_router
 from app.api.protected_routes import router as protected_router
 from app.api.tenant_routes import router as tenant_router
+from app.api.api_key_routes import router as api_key_router
+
+from app.middleware.api_key_middleware import api_key_guard
 
 app = FastAPI()
 
@@ -17,11 +20,18 @@ def startup_event():
 
 
 # ---------------------------
+# MIDDLEWARE (SECURITY LAYER)
+# ---------------------------
+app.middleware("http")(api_key_guard)
+
+
+# ---------------------------
 # ROUTES
 # ---------------------------
 app.include_router(auth_router)
 app.include_router(protected_router)
 app.include_router(tenant_router)
+app.include_router(api_key_router)
 
 
 # ---------------------------
