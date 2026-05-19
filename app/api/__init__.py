b383db82a62276a2dd835fd04c073e195d493575
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-19 08:08:51 +0700

    Import and aggregate API routers in __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,16 @@
 
+from .auth_routes import router as auth_router
+from .protected_routes import router as protected_router
+from .admin_routes import router as admin_router
+from .billing_routes import router as billing_router
+from .user_routes import router as user_router
+from .webhooks import router as webhooks_router
+
+all_routers = [
+    auth_router,
+    protected_router,
+    admin_router,
+    billing_router,
+    user_router,
+    webhooks_router,
+]
